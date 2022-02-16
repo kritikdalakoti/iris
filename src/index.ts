b22aaadbfae95d94b78b47ef258ce5125bb7236e
vikalp2502 <vikalpkaushik97@gmail.com>
2022-02-16 18:05:11 +0530

    add manual arithmetic operation logic in dashboard controller

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,46 @@
-import * as Configs from "./configurations";
-import * as Server from "./server";
-import { AppGlobalVariableInterface } from "./interfaces/app-global-variables-interface";
-import * as knex from 'knex';
-import { InfluxConfigInterface } from "./interfaces/influx-configuration-interface";
+import * as Configs from './configurations';
+import * as Server from './server';
+import knex from 'knex';
+import { AppGlobalVariableInterface } from './interfaces/app-global-variables-interface';
+import { InfluxConfigInterface } from './interfaces/influx-configuration-interface';
 const Influx = require('influx');
-var nodemailer = require("nodemailer");
+var nodemailer = require('nodemailer');
 
 console.log(`Running enviroment ${process.env.NODE_ENV || 'dev'}`);
 
 // Catch unhandling unexpected exceptions
 process.on('uncaughtException', (error: Error) => {
-    console.error(`uncaughtException ${error.message}`);
+	console.error(`uncaughtException ${error.message}`);
 });
 
 // Catch unhandling rejected promises
 process.on('unhandledRejection', (reason: any) => {
-    console.error(`unhandledRejection ${reason}`);
+	console.error(`unhandledRejection ${reason}`);
 });
 
 let confgis = Configs.getConfigurations();
 let influxConfig: InfluxConfigInterface = confgis.influxConfig;
 
 const start = async ({ config, globalVariables, myHost }) => {
-    try {
-
-        const server = await Server.init(config, globalVariables, myHost);
-        await server.start();
-        console.log('Server running at:', server.info.uri);
-    } catch (err) {
-        console.error('Error starting server: ', err.message);
-        throw err;
-    }
+	try {
+		const server = await Server.init(config, globalVariables, myHost);
+		await server.start();
+		console.log('Server running at:', server.info.uri);
+	} catch (err) {
+		console.error('Error starting server: ', err.message);
+		throw err;
+	}
 };
 
 let transporter = nodemailer.createTransport({
-    host: confgis.mailConfig.host,
-    secure: false, port: 587, tls: { rejectUnauthorized: false },
-    auth: {
-        user:  confgis.mailConfig.username, // generated gmail user
-        pass:  confgis.mailConfig.password, // generated gmail account password
-    }
+	host: confgis.mailConfig.host,
+	secure: false,
+	port: 587,
+	tls: { rejectUnauthorized: false },
+	auth: {
+		user: confgis.mailConfig.username, // generated gmail user
+		pass: confgis.mailConfig.password, // generated gmail account password
+	},
 });
 
 let globalVariables: AppGlobalVariableInterface = {};
@@ -50,9 +51,8 @@ globalVariables.awsS3 = confgis.awsS3;
 globalVariables.influx = new Influx.InfluxDB(influxConfig.connectionString);
 globalVariables.mailTransporter = transporter;
 
-
 start({
-    config: confgis.serverConfig,
-    globalVariables: globalVariables,
-    myHost: confgis.myHost
+	config: confgis.serverConfig,
+	globalVariables: globalVariables,
+	myHost: confgis.myHost,
 });
